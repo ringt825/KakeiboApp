4fd864622fa7e02ca13e4d2fdad05ea62712912b
Nachan <ringt825@gmail.com>
2023-03-06 20:11:42 +0900

    Initial Commit

---
+++ kakeibo/kakeiboApp.swift
@@ -0,0 +1,17 @@
+//
+//  kakeiboApp.swift
+//  kakeibo
+//
+//  Created by 金子奈々華 on 2023/03/06.
+//
+
+import SwiftUI
+
+@main
+struct kakeiboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
